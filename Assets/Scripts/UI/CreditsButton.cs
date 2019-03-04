8c77ab66a27effd5a4d3bedb5b4314d2b18cd0d7
Integraldx <jmh121463@gmail.com>
2019-03-04 12:42:18 +0900

    Credit button implementation

---
+++ Assets/Scripts/UI/CreditsButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsButton : MonoBehaviour {
+
+
+    public GameObject creditsImage; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void toggleCredits()
+    {
+        if(creditsImage.activeInHierarchy)
+        {
+            creditsImage.SetActive(false);
+        }
+        else
+        {
+            creditsImage.SetActive(true);
+        }
+    }
+}
