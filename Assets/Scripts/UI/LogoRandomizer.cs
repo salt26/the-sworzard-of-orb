6da55b801e03bb0a0e0f920dce6ba02a022697e0
Integraldx <jmh121463@gmail.com>
2019-03-04 09:52:32 +0900

    Logo orb keeps changing

---
+++ Assets/Scripts/UI/LogoRandomizer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogoRandomizer : MonoBehaviour {
+    public List<Sprite> orbs;
+    Image imageComponent;
+    int currentOrbImageIndex;
+    
+	// Use this for initialization
+	void Start () {
+        imageComponent = GetComponent<Image>();
+        currentOrbImageIndex = Random.Range(0, orbs.Count);
+        imageComponent.sprite = orbs[currentOrbImageIndex];
+        StartCoroutine(orbRandomizer());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    IEnumerator orbRandomizer()
+    {
+        int prev = currentOrbImageIndex;
+        while(prev == currentOrbImageIndex)
+        {
+            currentOrbImageIndex = Random.Range(0, orbs.Count);
+        }
+        imageComponent.sprite = orbs[currentOrbImageIndex];
+
+        yield return new WaitForSeconds(3.0f);
+
+        StartCoroutine(orbRandomizer());
+    }
+}
